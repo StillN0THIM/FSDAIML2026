13b12c81d430d3c9789ebeb244e9bcf074f330b3
him <kapoorhimanshu124@gmail.com>
2026-07-24 09:28:37 +0530

    added index files

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Hello using JS")
+
+// var a = 6
+// console.log("a="+a)
+
+// let a = 5
+// if (a<30){
+//     let a = 100
+//     console.log("a inside the block="+a)
+// }
+// console.log("a outside the block=" + a)
+
+// const sum = function(a,b){
+//     return a+b
+// }
+// const data = sum(20,20)
+// console.log("sum="+data)
+
+// const msg=(mymsg)=>{
+//     console.log("hi"+mymsg)
+// }
+// msg("welcome to fsd")
+
+// const sqrt = (n)=>Math.sqrt(n)
+// console.log(sqrt(25))
+
+// (()=>{console.log("hi")})();
