7c3b8374299b17c53b3830ccb6f778ba5f8f2a66
him <kapoorhimanshu124@gmail.com>
2026-07-24 10:10:54 +0530

    updated Index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Hello using JS")
+// console.log("Hello using JS")
 
 // var a = 6
 // console.log("a="+a)
@@ -25,3 +25,30 @@ console.log("Hello using JS")
 // console.log(sqrt(25))
 
 // (()=>{console.log("hi")})();
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function sumWithMsg(clbk, msg) {
+//     const result = clbk(5, 10);
+//     console.log(msg + ": " + result);
+// }
+
+// sumWithMsg(sum, "The sum is");
+
+function login(msg,error){
+    if(error){
+        console.log("error :"+error0)
+    }
+    else{
+        console.log("Welcome"+msg)
+    }
+}
+
+function loginVerification(username,password,clbk){
+    if(username=="hi40"&& password=="12345678"){
+        clbk("success",null);
+    }else{
+        clbk(null,"username or passward is incorrect")
+    }
+}
